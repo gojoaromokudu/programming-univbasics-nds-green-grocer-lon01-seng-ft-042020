a782dc897b8d5f617f51c136d68ae82945f9816d
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-08 20:43:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,7 @@ def consolidate_cart(cart)
  
  
  
- =begin
+ #=begin
  # pp cart
  # all_unique_items_with_count = [{:count => 1, :name => "TEMPEH"}]
  #cart_item_index = 0 
@@ -72,7 +72,7 @@ def consolidate_cart(cart)
 
  # pp all_unique_items_with_count
 #end
-=end
+#=end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
