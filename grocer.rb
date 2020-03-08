dcd7c0dc07e1a14c2575eb6a17bdffdf0e35e682
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-08 22:20:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -91,5 +91,10 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  final_cart = apply_clearance(couponed_cart)
+  total = 0 
+  
   
 end
