7bf7c785af2609b6e4c4624f27d76deb8aecc66b
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-08 21:40:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def apply_coupons(cart, coupons)
   coupon_index = 0 
   while coupon_index < coupons.length do 
     item_with_coupon = find_item_by_name_in_collection(coupons[coupon_index][:item], cart)
-   item_with_coupon_name "#{coupons"
+   item_with_coupon_name "#{coupons[coupon_index][:item]} W/COUPON"
   #  if item_with_coupon
       #rename the item 
       #also check the quantity
