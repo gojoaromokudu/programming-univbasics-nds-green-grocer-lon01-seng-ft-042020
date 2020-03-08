3867bb955a2059e42c99df16b5898595fd492a4a
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-08 20:48:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,6 +24,7 @@ def consolidate_cart(cart)
      new_cart_item[item_index][:count] += 1
    else 
      new_cart_item = cart[item_index]
+     pp cart[item_index]
      new_cart_item[item_index][:count] = 1
      new_cart << new_cart_item
    end
