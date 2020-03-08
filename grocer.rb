a25a6f3a85c8109b79acf5216662a5a4ae264e57
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-08 22:23:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -96,6 +96,7 @@ def checkout(cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0 
+  counter = 0
   while counter < final_cart.length
   total += final_cart[counter][:price] * final_cart[counter][:count]
   counter += 1
